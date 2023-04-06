87d4bb43154b45c79e74c0ba86fefcda902a0630
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-06 19:54:01 +0600

    html css and js file modified

---
+++ main.js
@@ -0,0 +1,36 @@
+
+import { MyBookList } from './modules/scritp.js';
+
+
+
+
+const list = document.querySelector('.list');
+const addNew = document.querySelector('.add-new');
+const contact = document.querySelector('.contact');
+const bookSection = document.querySelector('.book-section');
+const formContainer = document.querySelector('.form-container');
+const contactSection = document.querySelector('.cantact-section');
+
+list.addEventListener('click', () => {
+  bookSection.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactSection.style.display = 'none';
+  alert('hi vi');
+  console.log('hi viiikdsa')
+});
+
+addNew.addEventListener('click', () => {
+  bookSection.style.display = 'none';
+  formContainer.style.display = 'block';
+  contactSection.style.display = 'none';
+});
+
+contact.addEventListener('click', () => {
+  bookSection.style.display = 'none';
+  formContainer.style.display = 'none';
+  contactSection.style.display = 'block';
+});
+
+
+const myBookList = new MyBookList();
+myBookList();
