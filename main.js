2064efdd548598178d7d61bfb025461fdd036e79
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-09 22:10:07 +0600

    files edited

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 
 import { MyBookList } from './modules/scritp.js';
 
+import { DateTime } from './modules/luxon.js';
 
+
+const updateTime = () => {
+  const now = DateTime.now();
+  const options = { month: 'short', day: 'numeric', year: 'numeric' };
+  const date = now.toLocaleString(options);
+  const time = now.toLocaleString(DateTime.TIME_WITH_SECONDS);
+  document.getElementById('datetime').innerHTML = `${date} , ${time}`;
+};
+
+setInterval(updateTime, 1000);
+
+
+
+
+
+// const today = DateTime.now();
+// domElement.time.innerText = today.toLocaleString(DateTime.DATE_HUGE);
+
+// domElement.timeSection.appendChild(domElement.time);
+
+// Import the getCurrentDateTime and formatDateTime functions from the luxon-utils.js file
+// import { getCurrentDateTime, formatDateTime } from './luxon-utils.js';
+
+
+
+
+
+// const dateTime = document.querySelector('.dateTime');
+// const now = luxon.DateTime.now();
+// const timeFormate = dateTime.innerHTML=  now.toLocaleString(luxon.DateTime.DATETIME_FULL);
 
 
 const list = document.querySelector('.list');
@@ -32,3 +63,5 @@ contact.addEventListener('click', () => {
 
 const myBookList = new MyBookList();
 myBookList();
+
+
