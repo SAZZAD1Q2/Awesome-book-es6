fed620b787556f319060f442ac5e6108ce805ade
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-10 23:06:38 +0600

    files modified

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,18 +1,15 @@
 import { DateTime } from './luxon.js';
 
 const dateTime = () => {
+  const date = document.getElementById('dateTime');
 
+  setInterval(() => {
+    const now = DateTime.now();
 
-    const date = document.getElementById('dateTime');
-
-    setInterval(() => {
-      const now = DateTime.now();
-    
-      date.innerHTML = now.toLocaleString(
-        DateTime.DATETIME_MED_WITH_SECONDS,
-      );
-    }, 1000);
-
+    date.innerHTML = now.toLocaleString(
+      DateTime.DATETIME_MED_WITH_SECONDS,
+    );
+  }, 1000);
 };
 
 setInterval(dateTime, 1000);
